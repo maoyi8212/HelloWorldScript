4d59995d506cb35db4b9bdddc70128d51414d56a
maoyi <105657494@qq.com>
2016-07-19 21:58:43 +0800

    Update run.sh

--- src/script/run.sh
+++ src/script/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "HELLO WORLD"
-curl http://test1-test-frontend.cfapps.io/greeting >> helloworld.json
+#curl http://test1-test-frontend.cfapps.io/greeting >> helloworld.json
+curl http://$1.cfapps.io/greeting >> helloworld.json
